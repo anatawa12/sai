814f21d96194566f410a76794746425ed8f67168
anatawa12 <anatawa12@icloud.com>
2020-10-26 23:22:19 +0900

    add sync action

---
+++ tools/ci/linux/update-with-rhino.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+RHINO_REPO="https://github.com/mozilla/rhino.git"
+
+shallow_fetch_branch() {
+  git remote set-branches --add "$1" "$2"
+  git fetch "$1" "$2" --depth 1
+}
+
+main() {
+  set -eu
+  shallow_fetch_branch origin rhino-master
+  git checkout rhino-master
+  git remote add rhino "$RHINO_REPO"
+  shallow_fetch_branch rhino master
+  git reset --hard rhino/master
+  git remote rm rhino
+  git push origin rhino-master
+  if [ "$(git rev-parse origin/rhino-master)" = "$(git rev-parse rhino-master)" ]; then
+    echo "already up to date!"
+    return 0;
+  fi
+  gh pr create \
+    --base master \
+    --head rhino-master \
+    --title "sync rhino" \
+    --body "sync rhino" \
+    --fill
+}
+
+main
