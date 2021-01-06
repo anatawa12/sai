a4bc7ed37950834c984c623048672017db9fb15c
anatawa12 <anatawa12@icloud.com>
2021-01-06 11:35:25 +0900

    make catchVariable more specified type

--- stiasrc/com/anatawa12/sai/stia/TryInfo.kt
+++ stiasrc/com/anatawa12/sai/stia/TryInfo.kt
@@ -1,8 +1,8 @@
 package com.anatawa12.sai.stia
 
 class TryInfo {
-    var catchVariable: VariableId? = null
-    var finallyVariable: VariableId? = null
+    var catchVariable: VariableId.Internal? = null
+    var finallyVariable: VariableId.Internal? = null
 
     override fun toString(): String {
         return "TryInfo(catchVariable=$catchVariable, finallyVariable=$finallyVariable)"
